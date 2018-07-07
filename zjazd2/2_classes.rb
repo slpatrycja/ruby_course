0183eb76b3c50e44f6b6697e9ed32c5dd30d8600
slpatrycja <sl.patrycja.of@gmail.com>
2018-07-07 12:17:39 +0200

    classes

--- zjazd2/2_classes.rb
+++ zjazd2/2_classes.rb
@@ -25,6 +25,9 @@ end
 
 
 class Cat
+	def initialize(name, breed)
+		puts "Miau, my name is #{name}, I'm #{breed}"
+	end
 	def bring(item)
 		puts "Nie przynoszę #{item}"
 	end
@@ -34,6 +37,9 @@ class Cat
 end
 
 class Dog
+	def initialize(name, breed)
+		puts "Woof, my name is #{name}, I'm #{breed}"
+	end
 	def bring(item)
 		if item == "stick"
 			puts "Przynoszę #{item}"
@@ -46,8 +52,21 @@ class Dog
 	end
 end
 
-cat = Cat.new
-cat.bring("patyk")
+# cat = Cat.new('Teodor','scottish')
+# cat.bring("patyk")
 
-dog = Dog.new
-dog.bring("stick")
+# dog = Dog.new('Bailey', 'golden retriever')
+# dog.bring("stick")
+
+class Book
+	def initialize(title, nb_of_pages)
+		@title = title
+		@nb_of_pages = nb_of_pages
+	end
+	def show_book
+		puts "#{@title} has #{@nb_of_pages} pages"
+	end
+end
+
+harry_potter = Book.new("Harry Potter", 700)
+harry_potter.show_book
