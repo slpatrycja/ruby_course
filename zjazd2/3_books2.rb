fab2140f75f226b31a8fb14c4b040a9b04bf6b14
slpatrycja <sl.patrycja.of@gmail.com>
2018-07-07 13:34:11 +0200

    class methods

--- zjazd2/3_books2.rb
+++ zjazd2/3_books2.rb
@@ -1,5 +1,5 @@
 class Book
-	
+
 	  def initialize(title, number_of_pages)
 	    @title = title
 	    @number_of_pages = number_of_pages
@@ -13,6 +13,10 @@ class Book
 	  	@title
 	  end
 
+	  def title=(title)
+	  	@title = title
+	  end
+
 	  def number_of_pages
 	  	@number_of_pages
 	  end
@@ -24,7 +28,25 @@ Book.new("Pan Tadeusz", 300),
 Book.new("W pustyni i w puszczy", 220)
 ]
 
-def book_exist?(books, title)
-	books.any? { |book| book.title == title } 
+# def book_exist?(books, title)
+# 	books.any? { |book| book.title == title } 
+# end
+# puts book_exist?(books, 'vfvf')
+
+# book = books[0]
+# puts book
+# book.title = "Inny"
+# puts book
+
+def add_additional_info_to_books(books)
+	books.each { |book| book.title += ' (wiele stron)' if book.number_of_pages > 200 }
 end
-puts book_exist?(books, 'vfvf')
+
+# books_add = add_additional_info_to_books(books)
+# puts books_add
+
+def long_books(books)
+	books.select { |book| book.number_of_pages > 200 }.map { |book| book.title }
+end
+
+print long_books(books)
